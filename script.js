8957827ce89daf451862d1293b05744898939d70
francois80 <glavieux.francois@orange.fr>
2019-10-25 15:40:24 +0200

    correction

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function fAffiche(){
   var nb1 = document.getElementById("firstNumber").value;
   var nb2 = document.getElementById("secondNumber").value;
-  var result = Math.trunc(parseFloat(nb1)) % Math.trunc(parseFloat(nb2));
-  alert(result);
+  if((nb1 == '' || nb2 == '') || (isNaN(nb1) || isNaN(nb2))){
+      alert('saisir un nombre');
   }
+    else{
+      var result = parseFloat(nb1) % parseFloat(nb2);
+      alert(result);
+    }
+  }
+document.getElementById('submit').addEventListener("click",fAffiche);
